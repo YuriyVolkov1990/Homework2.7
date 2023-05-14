1fb5900c1ce02e4a13fd4e43e680145a88b0fa7e
Professional <new99kpicem@gmail.com>
2023-05-15 01:04:31 +0300

    prc

---
+++ src/main/java/com/employee/homework2_5/EmployeeController.java
@@ -0,0 +1,11 @@
+package com.employee.homework2_5;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+    @GetMapping("/test")
+    public
+
+}
--- src/main/java/com/employee/homework2_5/service/EmployeeService.java
+++ src/main/java/com/employee/homework2_5/service/EmployeeService.java
@@ -1,4 +1,34 @@
 package com.employee.homework2_5.service;
 
+import com.employee.homework2_5.exception.EmployeeStorageIsFullException;
+import com.employee.homework2_5.model.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+@Service
 public class EmployeeService {
+    private static final int size_limit = 5;
+    private final Collection<Employee> employees = new ArrayList<>(size_limit);
+    public Employee add(Employee employee) {
+        if (employees.size()>=size_limit) {
+            throw new EmployeeStorageIsFullException();
+        }
+        if (employees.contains())
+        employees.add(employee);
+        return employee;
+    }
+    public Employee find(String firstName, String lastName) {
+        for (Employee employee : employees) {
+            if (employee.getFirstName().equals(firstName) && employee.getLastName().equals(lastName)) {
+                return employee;
+            }
+        }
+        return null;
+    }
+    public Employee remove (String firstName, String lastName) {
+        Employee employee = find(firstName, lastName);
+        employees.remove(employee);
+        return employee;
+    }
 }
